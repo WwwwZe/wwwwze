2799ee89ebebc90c63706798215220156421d5a6
wangze <wangze@zmeng123.com>
2022-11-15 19:46:15 +0800

    init

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>虎</template>
+    虎涛虎虎虎
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Hu</template>
+
+    This is a
+    <a href="javascript;" target="_blank" rel="noopener">tiger</a>.
+    <br />
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Hu</template>
+    hbtt
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <CommunityIcon />
+    </template>
+    <template #heading>hb</template>
+    hbll
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>虎啊虎</template>
+    虎虎虎虎虎虎虎
+  </WelcomeItem>
+</template>
