666422f261c9acf851eb2a2cf061ae051981daf1
WwwwZe <45747623+WwwwZe@users.noreply.github.com>
2022-11-17 07:18:39 +0000

    Deploying to gh-pages from @ WwwwZe/wwwwze@8082c98c997db595a938a3935e889fafb9acbe2e 🚀

---
+++ assets/AboutView.c619de9d.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,a as o}from"./index.03d54b0c.js";const _={},c={class:"about"},n=o("h1",null,"This is an Tiger page",-1),a=[n];function i(r,d){return t(),s("div",c,a)}const u=e(_,[["render",i]]);export{u as default};
